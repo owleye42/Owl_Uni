5d3dc71453fcd57bb6772665aab7f878f8496737
【T.Oonuma 】 <taiki4205@gmail.com >
2018-02-22 14:28:15 +0900

    祝問題解決

---
+++ Assets/Xi_player.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Xi_player : MonoBehaviour {
+
+	// Use this for initialization
+
+    public bool moveflag = false;
+    public float speed;
+
+    public float testmove = 0;
+
+    public float angle = 0;
+
+    public int pos = 14;
+    public int f_pos = 0;//移動先判定用
+
+    //ズレ補正用
+    public float x_hantei = 0;
+    public float z_hantei = 0;
+
+    public int obj_status = 8;//管理スクリプト用　0なし　1～6サイコロ　7サイコロ（消滅猶予）8プレイヤ
+
+    public int x_vec = 0;
+    public int z_vec = 0;
+    public bool DDD;
+
+
+   // Vector3 c_pos;
+    public GameObject chara;
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 c_pos = transform.position;
+        StartCoroutine(MoveControl());
+        c_pos = new Vector3(pos % 6, 0, -(pos / 6));
+        transform.position = c_pos;
+    }
+
+    // Update is called once per frame
+    IEnumerator MoveControl()
+    {
+        while (true)
+        {
+            if (moveflag == false)
+            {
+                if (Input.GetKey("up"))
+                {
+                    if (pos >= 6)
+                    {
+                        while (true)
+                        {
+
+
+                            transform.position = transform.position + new Vector3(0, 0, 1 / speed);
+                            testmove += 1 / speed;
+                            if (testmove >= 1)
+                            {
+                                testmove = 0;
+                                break;
+                            }
+                            z_vec = 1;
+                            yield return null;
+
+                        }
+                        pos -= 6;
+                    }
+                }
+                if (Input.GetKey("down"))
+                {
+                    if (pos <= 29)
+                    {
+                        while (true)
+                        {
+                            transform.position = transform.position + new Vector3(0, 0, -1 / speed);
+                            testmove += 1 / speed;
+                            if (testmove >= 1)
+                            {
+                                testmove = 0;
+                                break;
+                            }
+                            z_vec = -1;
+                            yield return null;
+                        }
+                        pos += 6;
+                    }
+                }
+
+                if (Input.GetKey("right"))
+                {
+                    if ((pos + 1) % 6 != 0)
+                    {
+                        while (true)
+                        {
+                            transform.position = transform.position + new Vector3(1 / speed, 0, 0);
+                            testmove += 1 / speed;
+                            if (testmove >= 1)
+                            {
+                                testmove = 0;
+                                break;
+                            }
+                            x_vec = 1;
+                            yield return null;
+
+                        }
+                        pos += 1;
+                    }
+                }
+                if (Input.GetKey("left"))
+                {
+                    if (pos % 6 != 0)
+                    {
+                        while (true)
+                        {
+                            transform.position = transform.position + new Vector3(-1 / speed, 0, 0);
+                            testmove += 1 / speed;
+                            if (testmove >= 1)
+                            {
+                                testmove = 0;
+                                break;
+                            }
+                            x_vec = -1;
+                            yield return null;
+
+                        }
+                        pos -= 1;
+                    }
+                }
+                DDD = false;
+                x_vec = 0;
+                z_vec = 0;
+            }
+            if (Input.GetKey("space"))
+            {
+                transform.position = transform.position + new Vector3(0, 1 / speed, 0);
+            }
+
+            if (Input.anyKey && Input.GetKey("space") != true)
+            {
+              
+                moveflag = true;
+            }
+            else
+            {
+                x_hantei = transform.position.x - (int)transform.position.x;
+                z_hantei = transform.position.z - (int)transform.position.z;
+                moveflag = false;
+            }
+            //OnCollisionEnter();
+            // Debug.Log("毎フレームの処理");
+
+
+            if (transform.position.y > 0 && Input.GetKey("space") != true)
+            {
+                transform.position = transform.position + new Vector3(0, -1 / speed, 0);
+                Debug.Log("補正");
+            }
+            if (x_hantei != 0 || z_hantei != 0)
+            {
+                if (pos != 0)
+                {
+                    transform.position = new Vector3(pos % 6, 0, -(pos / 6));
+                    // Debug.Log("補正");
+                }
+                else
+                {
+                    transform.position = new Vector3(0, 0, 0);
+                    //  Debug.Log("補正");
+                }
+
+            }
+            if (transform.position.y < 0)
+            {
+                transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+            }
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/xi.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class xi : MonoBehaviour {
+    Vector3 rotatePoint = Vector3.zero;  //回転の中心
+    Vector3 rotateAxis = Vector3.zero;   //回転軸
+    float cubeAngle = 0f;                //回転角度
+
+    float cubeSizeHalf;                  //キューブの大きさの半分
+    bool isRotate = false;               //回転中に立つフラグ。回転中は入力を受け付けない
+
+     float x_hantei = 0;
+     float z_hantei = 0;
+
+    float movement = 0;
+
+    public int sai_info = 3;
+    public int kari_pos = 6; 
+    // bool
+    GameObject Pl;
+    void Start()
+    {
+        Pl = GameObject.Find("Player");
+        cubeSizeHalf = transform.localScale.x / 2f;
+        StartCoroutine(Control());
+        
+    }
+
+    void Update()
+    {
+            //return;
+
+       
+    }
+    IEnumerator Control()
+    {
+        
+        while (true) {
+         //   Controller Cl = Pl.GetComponent<Controller>();
+            
+            ///回転スクリプト
+            //回転中は入力を受け付けない
+            if (!isRotate)
+            {
+                if (Input.GetKey("w"))
+                {
+                    if (kari_pos >= 6)
+                    {
+                        rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, cubeSizeHalf);
+                        rotateAxis = new Vector3(1, 0, 0);
+                        kari_pos -= 6;
+                    }
+                }
+                if(Input.GetKey("s"))
+                {
+                    if (kari_pos <=29)
+                    {
+                        rotatePoint = transform.position + new Vector3(0f, -cubeSizeHalf, -cubeSizeHalf);
+                        rotateAxis = new Vector3(-1, 0, 0);
+                        kari_pos += 6;
+                    }
+                    
+                }
+                if (Input.GetKey("a"))
+                {
+                    if (kari_pos  % 6 != 0) { 
+                        rotatePoint = transform.position + new Vector3(-cubeSizeHalf, -cubeSizeHalf, 0f);
+                        rotateAxis = new Vector3(0, 0, 1);
+                        kari_pos -= 1;
+                    }
+                }
+                if (Input.GetKey("d"))
+                {
+                    if ((kari_pos + 1) % 6 != 0)
+                    {
+                        rotatePoint = transform.position + new Vector3(cubeSizeHalf, -cubeSizeHalf, 0f);
+                        rotateAxis = new Vector3(0, 0, -1);
+                        kari_pos += 1;
+                    }
+                }
+                // 入力がない時はコルーチンを呼び出さないようにする
+                if (rotatePoint != Vector3.zero) { StartCoroutine(MoveCube()); }
+                /*右上2.5　2.5
+                左下-2.5　-2.5*/
+                x_hantei = transform.position.x - (int)transform.position.x;
+                z_hantei = transform.position.z - (int)transform.position.z;
+                
+            }
+            //////////////////////////////////////////////////////////////////////////////////
+            //スライドスクリプト
+            //////////////////////////////////////////////////////////////////////////////////
+
+            if (Input.GetKey("t"))
+            {
+                if (kari_pos >= 6)
+                {
+
+                    transform.position = new Vector3(1, 0, 0);
+                    kari_pos -= 6;
+                }
+            }
+            if (Input.GetKey("g"))
+            {
+                if (kari_pos <= 29)
+                {
+
+                    transform.position = new Vector3(-1, 0, 0);
+                    kari_pos += 6;
+                }
+
+            }
+            if (Input.GetKey("f"))
+            {
+                if (kari_pos % 6 != 0)
+                {
+
+                    transform.position = new Vector3(0, 0, 1);
+                    kari_pos -= 1;
+                }
+            }
+            if (Input.GetKey("h"))
+            {
+                if ((kari_pos + 1) % 6 != 0)
+                {
+
+                    transform.position = new Vector3(0, 0, -1);
+                    kari_pos += 1;
+                }
+            }
+            yield return null;
+        }
+        
+    }
+
+              //  if (Input.GetKey(KeyCode.DownArrow))
+    //Cl.x_vec == 1;
+    IEnumerator MoveCube()
+    {
+        //回転中のフラグを立てる
+        isRotate = true;
+
+        //回転処理
+        float sumAngle = 0f; //angleの合計を保存
+        while (sumAngle < 90f)
+        {
+            cubeAngle = 10f; //ここを変えると回転速度が変わる
+            sumAngle += cubeAngle;
+
+            // 90度以上回転しないように値を制限
+            if (sumAngle > 90f)
+            {
+                cubeAngle -= sumAngle - 90f;
+            }
+            transform.RotateAround(rotatePoint, rotateAxis, cubeAngle);
+
+            yield return null;
+        }
+
+        if (x_hantei != 0 || z_hantei != 0)
+        {
+            if (kari_pos != 0)
+            {
+                transform.position = new Vector3(kari_pos % 6, 0, -(kari_pos / 6));
+                // Debug.Log("補正");
+            }
+            else
+            {
+                transform.position = new Vector3(0, 0, 0);
+                //  Debug.Log("補正");
+            }
+
+        }
+        //回転中のフラグを倒す
+        isRotate = false;
+        rotatePoint = Vector3.zero;
+        rotateAxis = Vector3.zero;
+
+        //四方にray
+        // if(Physics.Raycast(transform.position,))
+        yield break;
+    }
+   
+}
